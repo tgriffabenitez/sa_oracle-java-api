2952caf351eb93ada2148de9a1017facab57725f
tgriffabenitez <tgriffabenitez@gmail.com>
2023-06-01 19:42:11 -0300

    inicio del proyecto

---
+++ oracle-java/src/main/java/com/sistemasactivos/oraclejava/OracleJavaApplication.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.oraclejava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleJavaApplication.class, args);
+    }
+
+}
---
+++ oracle-java/src/test/java/com/sistemasactivos/oraclejava/OracleJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemasactivos.oraclejava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
